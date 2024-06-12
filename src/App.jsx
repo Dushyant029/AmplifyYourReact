1be8dee3e40a9a7117884e1f3e0b45e724ad40fe
K. Dushyant Reddy <dushireddy291@gmail.com>
2024-06-12 16:22:11 +0530

    push data

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import logo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello from V2</h1>
+          </header>
+      </div>
+    </>
+  )
+}
+
+export default App
